c83f4823e4026f65278e9659f867adda13b60a49
rm1644969-lang <rm1644969@gmail.com>
2026-09-22 19:08:11 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'max-win-shell-v1';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './maxwin-icon.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
